a49ef0a9ed0c533ee012d13bef00e49bb236b076
alfiankronus <a.malik@kronusasia.com>
2016-06-01 18:36:43 +0700

    initia commit

---
+++ www/js/app.js
@@ -0,0 +1,201 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic', 'ionic-material']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.angsuran', {
+        url: '/angsuran',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/angsuran.html',
+                controller: 'ListsCtrl'
+            }
+        }
+    })
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'AppCtrl'
+    })
+     .state('register', {
+        url: '/register',
+        templateUrl: 'templates/register.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.ink', {
+        url: '/ink',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ink.html',
+                controller: 'InkCtrl'
+            }
+        }
+    })
+
+    .state('app.motion', {
+        url: '/motion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/motion.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('app.components', {
+        url: '/components',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/components.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('app.profile', {
+        url: '/profile',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profile.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('app.pulsa', {
+        url: '/pulsa',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/pulsa.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('app.information', {
+        url: '/information',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/informasi.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+    
+
+    .state('app.list', {
+        url: '/list',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/list.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('app.tagihan', {
+        url: '/tagihan',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/tagihan.html'
+            }
+        }
+    })
+
+    .state('app.input', {
+        url: '/input',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/input.html'
+            }
+        }
+    })
+
+    .state('app.extensions', {
+        url: '/extensions',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/extensions.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+
+    .state('app.bpjs', {
+        url: '/bpjs',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/bpjs.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+    .state('app.bpjs-list', {
+        url: '/bpjs-list',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/bpjs-list.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+    .state('app.cc', {
+        url: '/cc',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/cc.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+    .state('app.cc-input', {
+        url: '/cc-input',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/cc-input.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+
+    .state('app.bayar', {
+        url: '/bayar',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/bayar.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+});
---
+++ www/js/controllers/AppCtrl.js
@@ -0,0 +1,54 @@
+﻿app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    var navIcons = document.getElementsByClassName('ion-navicon');
+    for (var i = 0; i < navIcons.length; i++) {
+        navIcons.addEventListener('click', function () {
+            this.classList.toggle('active');
+        });
+    }
+
+    // .fromTemplate() method
+    var template = '<ion-popover-view>' +
+                    '   <ion-header-bar>' +
+                    '       <h1 class="title">My Popover Title</h1>' +
+                    '   </ion-header-bar>' +
+                    '   <ion-content class="padding">' +
+                    '       My Popover Contents' +
+                    '   </ion-content>' +
+                    '</ion-popover-view>';
+
+    $scope.popover = $ionicPopover.fromTemplate(template, {
+        scope: $scope
+    });
+    $scope.closePopover = function () {
+        $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.popover.remove();
+    });
+
+    $scope.groups = [];
+      for (var i=0; i<2; i++) {
+        $scope.groups[i] = {
+          name: i,
+          items: []
+        };
+        for (var j=0; j<3; j++) {
+          $scope.groups[i].items.push(i + '-' + j);
+        }
+      }
+
+      $scope.toggleGroup = function(group) {
+        if ($scope.isGroupShown(group)) {
+          $scope.shownGroup = null;
+        } else {
+          $scope.shownGroup = group;
+        }
+      };
+      $scope.isGroupShown = function(group) {
+        return $scope.shownGroup === group;
+      };
+});
