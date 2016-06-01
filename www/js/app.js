6a40199a205b97b72d58450a6bee3ce52cbde196
Alfian Maulana Malik <alfian_ia2@yahoo.co.id>
2016-06-02 05:45:09 +0700

    add

--- www/js/app.js
+++ www/js/app.js
@@ -235,5 +235,5 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     ;
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/tukar');
+    $urlRouterProvider.otherwise('/app/information');
 });
--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,54 +1,104 @@
-﻿app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout) {
+﻿app.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $ionicPopup, $timeout,$location) {
     // Form data for the login modal
     $scope.loginData = {};
 
     var navIcons = document.getElementsByClassName('ion-navicon');
     for (var i = 0; i < navIcons.length; i++) {
-        navIcons.addEventListener('click', function () {
+        navIcons.addEventListener('click', function() {
             this.classList.toggle('active');
         });
     }
 
     // .fromTemplate() method
     var template = '<ion-popover-view>' +
-                    '   <ion-header-bar>' +
-                    '       <h1 class="title">My Popover Title</h1>' +
-                    '   </ion-header-bar>' +
-                    '   <ion-content class="padding">' +
-                    '       My Popover Contents' +
-                    '   </ion-content>' +
-                    '</ion-popover-view>';
+        '   <ion-header-bar>' +
+        '       <h1 class="title">My Popover Title</h1>' +
+        '   </ion-header-bar>' +
+        '   <ion-content class="padding">' +
+        '       My Popover Contents' +
+        '   </ion-content>' +
+        '</ion-popover-view>';
 
     $scope.popover = $ionicPopover.fromTemplate(template, {
         scope: $scope
     });
-    $scope.closePopover = function () {
+    $scope.closePopover = function() {
         $scope.popover.hide();
     };
     //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function () {
+    $scope.$on('$destroy', function() {
         $scope.popover.remove();
     });
 
     $scope.groups = [];
-      for (var i=0; i<2; i++) {
+    for (var i = 0; i < 2; i++) {
         $scope.groups[i] = {
-          name: i,
-          items: []
+            name: i,
+            items: []
         };
-        for (var j=0; j<3; j++) {
-          $scope.groups[i].items.push(i + '-' + j);
+        for (var j = 0; j < 3; j++) {
+            $scope.groups[i].items.push(i + '-' + j);
         }
-      }
+    }
 
-      $scope.toggleGroup = function(group) {
+    $scope.toggleGroup = function(group) {
         if ($scope.isGroupShown(group)) {
-          $scope.shownGroup = null;
+            $scope.shownGroup = null;
         } else {
-          $scope.shownGroup = group;
+            $scope.shownGroup = group;
         }
-      };
-      $scope.isGroupShown = function(group) {
+    };
+    $scope.isGroupShown = function(group) {
         return $scope.shownGroup === group;
-      };
+    };
+    $scope.data = {};
+    $scope.login = function() {
+        
+        var username = 'admin@admin.com';
+        var password = '12345';
+        if (($scope.data.username == username) && ($scope.data.password == password)) {
+            window.localStorage.setItem("username", username);
+            window.localStorage.setItem("password", password);
+            console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
+            // $state.go('app.profile');
+            window.location = "#/app/profile";
+            // $location.path('/app/profile');
+            // $window.location.href = '#/app/profile';
+            // window.location.assign("templates/profile.html");
+        } else {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login Gagal',
+                template: 'Masukan Username dan Password dengan benar'
+            });
+        }
+    }
+
+    $scope.isLoggedIn = function() {
+        if (window.localStorage.getItem("username") !== undefined && window.localStorage.getItem("password") !== undefined) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    $scope.isLoggedInClass = function() {
+        if (window.localStorage.getItem("username") !== undefined && window.localStorage.getItem("password") !== undefined) {
+            return 'hidden';
+        } else {
+            return '';
+        }
+    };
+
+    $scope.isLoggedInClassLogin = function() {
+        if (window.localStorage.getItem("username") !== undefined && window.localStorage.getItem("password") !== undefined) {
+            return '';
+        } else {
+            return 'hidden';
+        }
+    };
+
+    $scope.logout = function() {
+        window.localStorage.removeItem("username");
+        window.localStorage.removeItem("password");
+    };
 });
